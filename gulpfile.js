2b1d1cbda83209db4ca443d0ebe1709920a1616f
lovedlyl <lovedlyl@163.com>
2016-04-24 15:51:00 +0800

    完成第二章 文本

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// 引入模块
+var gulp = require("gulp"),
+    browserSync = require("browser-sync").create(),
+    reload = browserSync.reload,
+    stream = browserSync.stream,
+    concat = require("gulp-concat"),
+    pug = require("gulp-pug"),
+    rename = require("gulp-rename");
+
+// 合并库文件
+gulp.task("lib", function() {
+    // 样式
+    gulp.src(["bower_components/bootstrap/dist/css/bootstrap.min.css"])
+        .pipe(concat("bootstrap.css"))
+        .pipe(gulp.dest("."));
+    // 脚本
+    gulp.src(["bower_components/jquery/dist/jquery.min.js",
+            "bower_components/bootstrap/dist/js/bootstrap.min.js"
+        ])
+        .pipe(concat("bootstrap.js"))
+        .pipe(gulp.dest("."))
+});
+
+// 将pug文件转换为html文件
+gulp.task("html", function() {
+    gulp.src("index.pug")
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest("."))
+        .pipe(stream());
+});
+
+
+
+gulp.task("default", function() {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch(["*.pug", "index.html"], ["html", reload]);
+
+});
