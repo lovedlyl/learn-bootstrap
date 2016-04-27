e225709b4765e3ab51dac863418b05cbc72f4ada
lovedlyl <lovedlyl@163.com>
2016-04-27 15:44:49 +0800

    完成按钮章节

--- gulpfile.js
+++ gulpfile.js
@@ -8,27 +8,41 @@ var gulp = require("gulp"),
     rename = require("gulp-rename"),
     plumber = require("gulp-plumber");
 
+// gulp browser-sync gulp-concat gulp-plumber gulp-pug gulp-rename 
 // 合并库文件
 gulp.task("lib", function() {
     // 样式
     gulp.src(["bower_components/bootstrap/dist/css/bootstrap.min.css"])
         .pipe(concat("bootstrap.css"))
+        .pipe(gulp.dest("../lovedlyl.github.io/learn-bootstrap/styles"))
         .pipe(gulp.dest("dist/styles"));
+    // 字体文件
+    gulp.src(["bower_components/bootstrap/dist/fonts/*"])
+        .pipe(gulp.dest("../lovedlyl.github.io/learn-bootstrap/fonts"))
+        .pipe(gulp.dest("dist/fonts"))
+
     // 脚本
     gulp.src(["bower_components/jquery/dist/jquery.min.js",
             "bower_components/bootstrap/dist/js/bootstrap.min.js"
         ])
         .pipe(concat("bootstrap.js"))
+        .pipe(gulp.dest("../lovedlyl.github.io/learn-bootstrap/scripts"))
         .pipe(gulp.dest("dist/scripts"))
 });
 
+// 将图片导入github.io中
+gulp.task("image", function () {
+    gulp.src("dist/images/*")
+        .pipe(gulp.dest("../lovedlyl.github.io/learn-bootstrap/images"))
+})
 // 将pug文件转换为html文件
 gulp.task("convertPug", function() {
     gulp.src(["src/index.pug", "src/ch*.pug"])
         .pipe(plumber())
-        .pipe(pug({ pretty: true }))
+        .pipe(pug({ pretty: false }))
         .pipe(plumber.stop())
         .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest("../lovedlyl.github.io/learn-bootstrap"))
         .pipe(stream());
 });
 
